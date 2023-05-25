1f08a59ed15fb1ebda529a4f79f8b2276a4d6d77
User Kmusiclife02 <kmusiclife02@www5211.sakura.ne.jp>
2023-05-25 18:14:25 +0900

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title></title>
-<link href="https://cdn.jsdelivr.net/gh/kmusiclife/bs5-starter@master/dist/css/bootswatch-cosmo.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/gh/kmusiclife/bs5-starter@master/dist/css/main.css" rel="stylesheet">
 <?php wp_head() ?>
 </head>
 <body>
