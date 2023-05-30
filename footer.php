be199d7df473caf698d1d55006055244aa6c6910
User Kmusiclife02 <kmusiclife02@www5211.sakura.ne.jp>
2023-05-31 02:19:43 +0900

    Update footer.php master->main

--- footer.php
+++ footer.php
@@ -57,8 +57,8 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/gh/kmusiclife/lux.js@master/lux.min.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/kmusiclife/bs5-starter@master/dist/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/kmusiclife/lux.js@main/lux.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/kmusiclife/bs5-starter@mmain/dist/js/main.js"></script>
 <?php wp_footer() ?>
 </body>
 </html>
