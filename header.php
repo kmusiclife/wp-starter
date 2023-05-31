02dbf9e7d6f4f11b11b5efc58cc46fad44f820d4
User Kmusiclife02 <kmusiclife02@www5211.sakura.ne.jp>
2023-05-31 19:08:37 +0900

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title></title>
 <link href="https://cdn.jsdelivr.net/gh/kmusiclife/bs5-starter@main/dist/css/main.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/gh/kmusiclife/bs5-content@main/dist/css/main.css" rel="stylesheet">
 <?php wp_head() ?>
 </head>
 <body>
